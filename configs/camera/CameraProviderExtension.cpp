cfc1681be3acc848314fdf156840e1b3e8f69922
bengris32 <bengris32@protonmail.ch>
2024-10-08 07:29:59 +0000

    peridot: camera: Implement setTorchModeExt
    Change-Id: Id61420be75b7efd1d13a4b0ee1d103ebd3835516
    Signed-off-by: electimon <electimon@gmail.com>
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- configs/camera/CameraProviderExtension.cpp
+++ configs/camera/CameraProviderExtension.cpp
@@ -45,7 +45,7 @@ bool supportsTorchStrengthControlExt() {
 }
 
 bool supportsSetTorchModeExt() {
-    return true;
+    return false;
 }
 
 int32_t getTorchDefaultStrengthLevelExt() {
@@ -74,4 +74,9 @@ void setTorchStrengthLevelExt(int32_t torchStrength) {
     }
     if (torchStrength > 0)
         set(TOGGLE_SWITCH, 255);
+}
+
+void setTorchModeExt(bool enabled) {
+    int32_t strength = getTorchDefaultStrengthLevelExt();
+    setTorchStrengthLevelExt(enabled ? strength : 0);
 }
