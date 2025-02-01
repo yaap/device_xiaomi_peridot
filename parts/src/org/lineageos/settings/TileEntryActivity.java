5a7c3604d2d1c646114adef1a031bade9200ac64
Guidix <guidixm@gmail.com>
2025-02-01 22:51:31 +0200

    peridot: parts: Redirect Saturation Tile long click to Saturation Settings Activity
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- parts/src/org/lineageos/settings/TileEntryActivity.java
+++ parts/src/org/lineageos/settings/TileEntryActivity.java
@@ -29,12 +29,14 @@ import androidx.annotation.Nullable;
 import org.lineageos.settings.refreshrate.RefreshActivity;
 import org.lineageos.settings.touchsampling.TouchSamplingSettingsActivity;
 import org.lineageos.settings.thermal.ThermalSettingsActivity;
+import org.lineageos.settings.saturation.SaturationActivity;
 
 public class TileEntryActivity extends Activity {
     private static final String TAG = "TileEntryActivity";
     private static final String REFRESH_TILE = "org.lineageos.settings.refreshrate.RefreshTileService";
     private static final String HTSR_TILE = "org.lineageos.settings.touchsampling.TouchSamplingTileService";
     private static final String THERMAL_TILE = "org.lineageos.settings.thermal.ThermalTileService";
+    private static final String SATURATION_TILE = "org.lineageos.settings.saturation.SaturationTileService";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -55,6 +57,8 @@ public class TileEntryActivity extends Activity {
             intent = new Intent(this, TouchSamplingSettingsActivity.class);
         } else if (THERMAL_TILE.equals(sourceClassName)) {
             intent = new Intent(this, ThermalSettingsActivity.class);
+        } else if (SATURATION_TILE.equals(sourceClassName)) {
+            intent = new Intent(this, SaturationActivity.class);
         } else {
             Log.e(TAG, "Unknown tile: " + sourceClassName);
             finish();
