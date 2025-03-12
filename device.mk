f9549ff7e07a2fa39d57bc3c11ad635391b3ba59
Yan Yan <evitayan@google.com>
2025-03-12 09:47:03 +0530

    peridot: Add feature flag for IPSEC_TUNNEL_MIGRATION
    This commit adds the IPSEC_TUNNEL_MIGRATION feature flag,
    with XFRM_MIGRATE used as the implementation
    
    Bug: 273379472
    Test: build, flash and verify with "adb shell pm list features"
    Test: atest IpSecManagerTunnelTest
    Change-Id: Id2ee3e65183e32ea63f6251ba921e1e4c6b09ed0
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -422,7 +422,8 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload-V1-ndk.vendor
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
 
 # NFC
 PRODUCT_PACKAGES += \
